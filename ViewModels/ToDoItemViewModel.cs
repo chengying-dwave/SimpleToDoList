10a388b6373574a785d0a8384dda4b1ef8c06608
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-03 17:39:22 +0800

    feat: add ToDoItemViewModel

---
+++ ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SimpleToDoList.ViewModels
+{
+    /// <summary>
+    /// This is a ViewModel which represents a <see cref="Models.ToDoItem"/>
+    /// </summary>
+    public partial class ToDoItemViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Gets or sets the checked status of each item
+        /// </summary>
+        [ObservableProperty]
+        private bool _isChecked;
+
+        /// <summary>
+        /// Gets or sets the content of the to-do item
+        /// </summary>
+        [ObservableProperty]
+        private string? _content;
+    }
+}
