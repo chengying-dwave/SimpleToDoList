87854756edf03ef48c11d8b58bffaf82671affb7
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-04 13:42:49 +0800

    feat: add ToDoItemViewModel into Main

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,15 @@
-﻿namespace SimpleToDoList.ViewModels;
+﻿using System.Collections.ObjectModel;
+
+namespace SimpleToDoList.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    /// <summary>
+    /// Gets a collection of <see cref="ToDoItem"/> which allows adding and removing items
+    /// </summary>
+    public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = new ObservableCollection<ToDoItemViewModel>();
 }
