2258044e5b5b92ad5d092a7a4da85f13ffd330fa
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-04 13:55:40 +0800

    feat: add AddItemCommand related

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace SimpleToDoList.ViewModels;
 
-public class MainWindowViewModel : ViewModelBase
+public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
@@ -12,4 +14,29 @@ public class MainWindowViewModel : ViewModelBase
     /// Gets a collection of <see cref="ToDoItem"/> which allows adding and removing items
     /// </summary>
     public ObservableCollection<ToDoItemViewModel> ToDoItems { get; } = new ObservableCollection<ToDoItemViewModel>();
+
+    /// <summary>
+    /// Gets or set the content for new Items to add. If this string is not empty, the AddItemCommand will be enabled automatically
+    /// </summary>
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(AddItemCommand))] // This attribute will invalidate the command each time this property changes
+    private string? _newItemContent;
+
+    /// <summary>
+    /// Returns if a new Item can be added. We require to have the NewItem some Text
+    /// </summary>
+    private bool CanAddItem() => !string.IsNullOrWhiteSpace(NewItemContent);
+
+    /// <summary>
+    /// This command is used to add a new Item to the List
+    /// </summary>
+    [RelayCommand(CanExecute = nameof(CanAddItem))]
+    private void AddItem()
+    {
+        // Add a new item to the list
+        ToDoItems.Add(new ToDoItemViewModel() { Content = NewItemContent });
+
+        // reset the NewItemContent
+        NewItemContent = null;
+    }
 }
