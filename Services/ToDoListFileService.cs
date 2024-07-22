4dbe7a45b50f69402a201e34e7c057103c97ac9b
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-22 17:58:04 +0800

    feat: implement SaveToFileAsync

--- Services/ToDoListFileService.cs
+++ Services/ToDoListFileService.cs
@@ -1,14 +1,32 @@
 ﻿using SimpleToDoList.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SimpleToDoList.Services;
 
 public static class ToDoListFileService
 {
-    internal static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemsToSave)
+    // This is a hard coded path to the file. It may not be available on every platform. In your real world App you may
+    // want to make this configurable
+    private static string _jsonFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Avalonia.SimpleToDoList", "MyToDoList.txt");
+
+    /// <summary>
+    /// Stores the given items into a disc
+    /// </summary>
+    /// <param name="itemsToSave"></param>
+    /// <returns></returns>
+    public static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemsToSave)
     {
-        throw new NotImplementedException();
+        // Ensure all directories exists
+        Directory.CreateDirectory(Path.GetDirectoryName(_jsonFileName)!);
+
+        // We use a FileStream to write all items to disc
+        using (var fs = File.Create(_jsonFileName))
+        {
+            await JsonSerializer.SerializeAsync(fs, itemsToSave);
+        }
     }
 }
