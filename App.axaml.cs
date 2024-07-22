f97c555a6310400554ad7e64274632f572d2b2dd
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-22 17:42:59 +0800

    feat: handle ShutdownRequested

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using SimpleToDoList.Services;
 using SimpleToDoList.ViewModels;
 using SimpleToDoList.Views;
+using System.Linq;
 
 namespace SimpleToDoList;
 
@@ -11,6 +13,8 @@ public partial class App : Application
     // This is a reference to our MainViewModel which we use to save the list on shutdown. You can also use Dependency Injection
     // in your App.
     private readonly MainWindowViewModel _mainWindowViewModel = new MainWindowViewModel();
+
+    private bool _canClose; // This flag is used to check if window is allowed to close
 
     public override void Initialize()
     {
@@ -25,8 +29,32 @@ public partial class App : Application
             {
                 DataContext = _mainWindowViewModel, // Remember to change this line to use our private reference to the MainViewModel
             };
+
+            // Listen to the ShutdownRequested-event
+            desktop.ShutdownRequested += DesktopShutdownRequested;
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    // We want to save our ToDoList before we actually shutdown the App. As File I/O is async, we need to wait until file is closed
+    // before we can actually close this window
+    private async void DesktopShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+    {
+        e.Cancel = !_canClose; // cancel closing event first time
+
+        if (!_canClose)
+        {
+            // To save the items, we map them to the ToDoItem-Model which is better suited for I/O operations
+            var itemsToSave = _mainWindowViewModel.ToDoItems.Select(item => item.GetToDoItem());
+            await ToDoListFileService.SaveToFileAsync(itemsToSave);
+
+            // Set _canClose to true and Close this Window again
+            _canClose = true;
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
     }
 }
---
+++ Services/ToDoListFileService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleToDoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleToDoList.Services;
+
+public static class ToDoListFileService
+{
+    internal static async Task SaveToFileAsync(IEnumerable<ToDoItem> itemsToSave)
+    {
+        throw new NotImplementedException();
+    }
+}
